42b7d97aa76f383121f2f350ed1a6bbedc3e4468
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-22 15:35:08 -0800

    select the session property on the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  session$;
+
+  constructor(private _store: Store<any>){
+    this.session$ = _store.select('session');
+  }
+
+  ngOnInit(){}
 }
