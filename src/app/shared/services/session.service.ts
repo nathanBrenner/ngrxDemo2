ebb4b0b479a5b7c44a6018cb482b9154c2d69a86
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-22 16:26:50 -0800

    write the session service

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
 
+import { HttpWrapperService } from './http-wrapper.service';
+import { SESSION_ACTIONS } from '../reducers/session.reducer';
+import { HttpParams } from '../models/httpParams.interface';
+
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+  constructor(private httpWrapperService: HttpWrapperService) {}
+
+  getUser(){
+    let getParams: HttpParams = {
+      auth: true,
+      errorActionType: SESSION_ACTIONS.GET_USER.FAILURE,
+      responseObject: 'user',
+      successActionType: SESSION_ACTIONS.GET_USER.SUCCESS,
+      uri: 'user'
+    }
+    return this.httpWrapperService.get(getParams);
+  }
+
+  loginUser(payload: {email: string, password: string}){
+    let postParams: HttpParams = {
+      auth: false,
+      errorActionType: SESSION_ACTIONS.LOGIN_USER.FAILURE,
+      payload: payload,
+      responseObject: 'account',
+      successActionType: SESSION_ACTIONS.LOGIN_USER.SUCCESS,
+      uri: 'user'
+    }
+    return this.httpWrapperService.post(postParams);
+  }
+
+  registerUser(payload: {username: string, email: string, password: string, verify_password: string}){
+    let postParams: HttpParams = {
+      auth: false,
+      errorActionType: SESSION_ACTIONS.REGISTER_USER.FAILURE,
+      payload: payload,
+      responseObject: 'account',
+      successActionType: SESSION_ACTIONS.REGISTER_USER.SUCCESS,
+      uri: 'users'
+    }
+    return this.httpWrapperService.post(postParams)
+  }
 
 }
