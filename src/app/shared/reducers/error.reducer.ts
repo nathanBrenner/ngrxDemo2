9a44ac433c3700df70d3bf6030bee13974d8c08b
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-24 20:05:11 -0800

    added another note

--- src/app/shared/reducers/error.reducer.ts
+++ src/app/shared/reducers/error.reducer.ts
@@ -12,7 +12,7 @@ export const errorReducer: ActionReducer<Error[]> = (state: Error[] = [], {type,
 		case ERROR_ACTIONS.REPORT_ERROR:
 			let id = Math.floor(Math.random() * 1000);
 			payload.id = id;
-			return [...state, payload]
+			return [...state, payload] // spread operator
 		case ERROR_ACTIONS.REMOVE_ERROR:
 			return state.filter(error => error.id != payload.id);
 		default:
